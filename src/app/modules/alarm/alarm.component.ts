a73c06b8a8a16974273a1b4f52551e681f4d0c78
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2025-02-10 21:40:14 -0700

    Creada plantilla base del contador, hace cuenta progresiva por el momento, se tuvo que adecuar la posición de los números del contador porque la fuente que escogí no es monoespaciada

--- src/app/modules/alarm/alarm.component.ts
+++ src/app/modules/alarm/alarm.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-alarm',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './alarm.component.html',
   styleUrl: './alarm.component.scss'
 })
 export class AlarmComponent {
+
+  seconds:number = 5;
+  tenseconds:number = 5;
+  realseconds:number = 55;
+  minutes:number = 0;
+  tenminutes:number = 0;
+  isFirstNine:boolean=true;
+  isFirstNineMinutes:boolean=true;
+  
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.runTimer()
+  }
+
+
+  runTimer(){
+
+    setInterval(() => {
+      this.seconds +=1;
+      this.realseconds+=1;
+
+      if(this.seconds>=10){
+        this.seconds = 0;
+        this.tenseconds += 1;
+      }
+
+      if(this.realseconds>59){
+        this.seconds = 0;
+        this.tenseconds = 0;
+        this.realseconds = 0;
+        this.minutes +=1;
+      }
+    }, 1000);
+
+  }
+
 
 }
