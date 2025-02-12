6c3915452b0355e434b28cdad73a7a2927068e0d
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2025-02-11 22:52:51 -0700

    cambio del setInterval normal por Subscription de rxjs para mejor rendimiento

--- src/app/modules/alarm/alarm.component.ts
+++ src/app/modules/alarm/alarm.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-alarm',
@@ -10,34 +11,38 @@ import { Component } from '@angular/core';
 })
 export class AlarmComponent {
 
-  seconds:number = 0;
-  tenseconds:number = 0;
+
   realseconds:number = 0;
   realminutes:number = 2;
-  minutes:number = 0;
-  tenminutes:number = 0;
-  
-  /**
-   * El plan de los números es el siguiente:
-   * como la vista es de 00:00, seconds se encargaria de los 9 digitos del lado derecho (00:01)
-   * tenseconds de las decenas de segundos (00:10), misma lógica para los minutos
-   * 
-   * Esto se hace porque cada 0 tiene su propio ancho designado debido a que la fuente no es monoespaciada
-   * 
-   * como cada variable solo almacenara una unidad, realseconds almacenara los verdaderos segundos que pasan del 0 al 59
-   * y con este valor es con el que se van a hacer las condiciones, lo mismo con realminutes
-   */
+
+
+  //Realtime es todos los minutos en segundos para hacer la conversion
+  //del porcentaje de la gráfica que se va a ir reduciendo
+  realtime: number = 120;
+  progressvalue: number = 0;
+  percentage: number = 100;
+
+  private subscription!: Subscription;
+
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.runTimer()
+    this.start()
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 
-  runTimer(){
-    setInterval(() => {
-    /* Empiezas con segundos en 0 y unos minutos establecidos, al empezar
+  start(){
+    this.subscription = interval(1000).subscribe(() => {
+          /* Empiezas con segundos en 0 y unos minutos establecidos, al empezar
     tienes que comprobar que haya 1 minuto como mínimo y 0 segundos */
     if (this.realminutes>0 && this.realseconds == 0) {
       this.realminutes -= 1;
@@ -48,31 +53,7 @@ export class AlarmComponent {
     //Resta un segundo
     this.realseconds -=1;
 
-    }, 1000);
-
+    })
   }
-  /**
-   * Contador progresivo
-   */
-  runProgressiveTimer(){
-    setInterval(() => {
-      this.seconds +=1;
-      this.realseconds+=1;
-
-      if(this.seconds>=10){
-        this.seconds = 0;
-        this.tenseconds += 1;
-      }
-
-      if(this.realseconds>59){
-        this.seconds = 0;
-        this.tenseconds = 0;
-        this.realseconds = 0;
-        this.minutes +=1;
-      }
-    }, 1000);
-
-  }
-
 
 }
