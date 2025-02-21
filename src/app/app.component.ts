7ff3f57cd8fc52d37569655c8fa0866bfa0a5dce
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2025-02-20 20:16:15 -0700

    cambio del nombre de boxing alarm a boxing timer, los datos que estan en el formulario estan predefinidos con un estandar para entrenar de 99 rounds (el maximo), de 3 minutos de entrenamiento por 1 minuto de descanso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AlarmComponent } from './modules/alarm/alarm.component';
-import { MenuComponent } from './modules/menu/menu.component';
 import { CommonModule } from '@angular/common';
 import { FormDataService } from './services/form-data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AlarmComponent,MenuComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'boxingalarm';
+  title = 'boxingtimer';
 
   constructor(private dataService:FormDataService){}
 
